3210913458da1303f799a11c335446294b952697
Juan Andres Orozco <juanxxi2015@gmail.com>
2023-06-23 08:54:44 -0500

    Finally pagination is shown in Tickets
    Pagination is shown in tickets

--- MQTT.Web/wwwroot/js/Views/Tickets/tickets.js
+++ MQTT.Web/wwwroot/js/Views/Tickets/tickets.js
@@ -1,10 +1,16 @@
 ﻿var dataHideColums, dataSearchColumns;
-
 $(document).ready(function () {
     createElemntsTimes();
     multiSelect();
     drodownDataSearch(columnsSearch, 'CustomName', 'searchParam');
 });
+
+//function exportToExcel() {
+//    var table = document.getElementById("table");
+//    var wb = XLSX.utils.table_to_book(table, { sheet: "Sheet 1" });
+//    var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+//    saveAs(new Blob([wbout], { type: 'application/octet-stream' }), 'tickets.xlsx');
+//}
 
 
 function validateDates() {
@@ -18,69 +24,17 @@ function validateDates() {
             title: 'Debe seleccionar la fecha',
         });
     } else {
-        ServiceGetTickets();
+        ServiceGetMessages();
     }
 }
-function ServiceGetMessages() {
-    Swal.fire({
-        title: 'Loading...',
-        allowOutsideClick: false,
-        showConfirmButton: false,
-        onBeforeOpen: (modal) => {
-            modal.showLoading();
-            modal.disableCloseButton();
-        }
-    });
 
-    var data = {
-        startDate: $("#dtpStart").val(),
-        endDate: $("#dtpEnd").val(),
-    };
-
-    $.ajax({
-        data: data,
-        type: "POST",
-        dataType: "json",
-        url: '/Messages/Search',
-    }).then(response => JSON.parse(JSON.stringify(response)))
-        .then(data => {
-            // Hide loading modal
-            Swal.close();
-
-            if (data.dataMessages.length == 0) {
-                noData();
-                return;
-            }
-            filtersData = data.filters;
-            dropdowns.value = null;
-            multiSelectInput.value = null;
-            dropdowns.enabled = false;
-            multiSelectInput.enabled = false;
-            const btn = document.getElementById('button-filter');
-            btn.disabled = false;
-            dateDocuments = $("#dtpStart").val() + " " + $("#dtpEnd").val();
-            let dataColumns = setColums(data.dataMessages, columnsToHide);
-            let exportFunctions = addFnctionsGrid(['Excel', 'Csv']);
-            dataColumns = addCommandsGridDetails(dataColumns);
-            dropdowns.enabled = true;
-            dataGridSave = data.dataMessages;
-            setGrid(data.dataMessages, dataColumns, exportFunctions);
-        })
-        .catch(error => {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: error.name + ': ' + error.message
-            });
-        })
-        .then(response => console.log('Success:', response));
-}
 function ServiceGetTickets() {
     var startDate = $('#dtpStart').val();
     var endDate = $('#dtpEnd').val();
     var max = 0;
     var componente = $('#componente').val();
-    var data = { startDate: startDate, endDate: endDate, max: max, componente: componente }
+    console.log("idComponente: " + componente);
+    console.log("Max: " + max);
 
     Swal.fire({
         title: 'Cargando...',
@@ -94,31 +48,7 @@ function ServiceGetTickets() {
     $.ajax({
         type: "GET",
         url: "/Tickets/GetTickets",
-        data: data
-    }).then(data => {
-        // Hide loading modal
-        Swal.close();
-
-        if (data.dataMessages.length == 0) {
-            noData();
-            return;
-        }
-        filtersData = data.filters;
-        dropdowns.value = null;
-        multiSelectInput.value = null;
-        dropdowns.enabled = false;
-        multiSelectInput.enabled = false;
-        const btn = document.getElementById('button-filter');
-        btn.disabled = false;
-        dateDocuments = $("#dtpStart").val() + " " + $("#dtpEnd").val();
-        let dataColumns = setColums(data.dataMessages, columnsToHide);
-        let exportFunctions = addFnctionsGrid(['Excel', 'Csv']);
-        dataColumns = addCommandsGridDetails(dataColumns);
-        dropdowns.enabled = true;
-        dataGridSave = data.dataMessages;
-        setGrid(data.dataMessages, dataColumns, exportFunctions);
-
-    })
+        data: { startDate: startDate, endDate: endDate, max: max, componente: componente },
         success: function (response) {
             Swal.close();
             var tbody = $('#table tbody');
@@ -162,6 +92,126 @@ function ServiceGetTickets() {
         }
     });
 }
+
+function showMoreInformation(idTicket) {
+    $.ajax({
+        url: '/Tickets/consultarTicket?idTicket=' + idTicket,
+        data: { idTicket: idTicket },
+        success: function (ticket) {
+
+            var html = '<table style="font-family: Arial, sans-serif; width: 100%; border-collapse: collapse;">';
+
+            $.each(ticket, function (key, value) {
+
+                html += '<tr>';
+                html += '<td style="text-align: left; padding: 8px; border: 2px solid #7f69a5; white-space: nowrap; font-weight: bold; font-size: 14px; background-color: rgba(127, 105, 165, 0.3);">' + key + ':</td>';
+                html += '<td style="text-align: left; padding: 8px; border: 2px solid #7f69a5; font-size: 13px;">' + value + '</td>';
+                html += '</tr>';
+            });
+
+            html += '</table>';
+
+            Swal.fire({
+                title: 'Información del Ticket',
+                html: html,
+                confirmButtonText: 'Cerrar',
+                showCloseButton: true,
+                showConfirmButton: false,
+                customClass: {
+                    container: 'swal-wide',
+                },
+                width: '50%',
+                padding: '2rem',
+                backdrop: true,
+                allowOutsideClick: true,
+                allowEscapeKey: false,
+            });
+        },
+        error: function () {
+            Swal.fire('Error', 'No se pudo obtener la información del ticket', 'error');
+        }
+    });
+}
+
+function exportToExcel() {
+    var table = document.getElementById("table");
+    var headers = table.getElementsByTagName("th");
+    var rows = table.getElementsByTagName("tr");
+
+    // Clonar la tabla para preservar la vista HTML original
+    var clonedTable = table.cloneNode(true);
+
+    // Eliminar el título de la primera columna en las cabeceras clonadas
+    var clonedHeaders = clonedTable.getElementsByTagName("th");
+    if (clonedHeaders.length > 0) {
+        clonedHeaders[0].parentNode.removeChild(clonedHeaders[0]);
+    }
+
+    // Eliminar la primera columna de cada fila clonada (excepto las cabeceras)
+    var clonedRows = clonedTable.getElementsByTagName("tr");
+    for (var i = 0; i < clonedRows.length; i++) {
+        var clonedCells = clonedRows[i].getElementsByTagName("td");
+        if (clonedCells.length > 0) {
+            clonedRows[i].removeChild(clonedCells[0]);
+        }
+    }
+
+    // Crear el libro de Excel sin la primera columna y el título
+    var wb = XLSX.utils.table_to_book(clonedTable, { sheet: "Tickets" });
+    var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    saveAs(new Blob([wbout], { type: 'application/octet-stream' }), 'tickets.xlsx');
+}
+
+/*************************************************************************************************** */
+function ServiceGetMessages() {
+    var startDate = $('#dtpStart').val();
+    var endDate = $('#dtpEnd').val();
+    var max = 0;
+    var componente = $('#componente').val();
+    console.log("idComponente: " + componente);
+    console.log("Max: " + max);
+
+    Swal.fire({
+        title: 'Cargando...',
+        allowOutsideClick: false,
+        showConfirmButton: false,
+        onBeforeOpen: (modal) => {
+            modal.showLoading();
+            modal.disableCloseButton();
+        }
+    });
+    $.ajax({
+        type: "GET",
+        url: "/Tickets/GetTickets",
+        data: { startDate: startDate, endDate: endDate, max: max, componente: componente },
+        
+    }).then(response => JSON.parse(JSON.stringify(response)))
+        .then(data => {
+        
+            Swal.close();
+            console.log(data)
+            if (data.length == 0) {
+                noData();
+                return;
+            } else {
+                let dataColumns = setColums(data, null);
+                let exportFunctions = addFnctionsGrid(['Excel', 'Csv']);
+                dataColumns = addCommandsGridDetails(dataColumns);
+                dataGridSave = data;
+                setGrid(data, dataColumns, exportFunctions);
+            }    
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error.name + ': ' + error.message
+            });
+        })
+        .then(response => console.log('Success:', response));
+}
+
+
 
 const targetEl = document.getElementById('dropdownInformation');
 
